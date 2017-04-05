9800dda78545cf03e7f653708b8e991ee72ae806
kamilsutkowki <you@example.com>
2017-04-05 11:20:34 +0200

    not work

--- app/app.js
+++ app/app.js
@@ -1,6 +1,17 @@
 
 function findMin(array)
 {
+var min = array[0];
+for(i=0;i<array.length;i++){
+    if(typeof(array[i]!="number")){
+        return false;
+    }
+}
+for (i=0;i<array.length[0];i++){
+    if(min<array[i]){
+        min = array[i]
+    }
+}
 
 }
 module.exports = {
