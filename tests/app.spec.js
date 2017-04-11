397f40028dad236be6a978056aa98485815a4862
kamilsutkowki <you@example.com>
2017-04-11 19:06:17 +0200

    refactor test

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,12 +1,16 @@
 var expect = require('chai').expect;
 var app = require('../app/app');
 
-it('should find max element of array', function()
+describe('when parameters are correct', function()
 {
-    expect(app.findMin([5,2,3,4,1,9,8,7,6])).to.eql(1);
+    it('should find min element of array', function() {
+        expect(app.findMin([5, 2, 3, 4, 1, 9, 8, 7, 6])).to.eql(1);
+    });
 });
 
-it('should return false if any of elements is not a number', function()
-{
-    expect(app.findMin([1,2,3,'x'])).to.eql(false);
+    describe('when parameters are negative numbers', function ()
+    {
+        it('should return false if any of elements is not a number', function() {
+            expect(app.findMin([1, 2, 3, 'x'])).to.eql(false);
+        })
 });
